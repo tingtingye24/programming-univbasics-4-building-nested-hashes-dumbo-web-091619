5cdc417a1f73b01f9c8d0be2989f6c016c377a36
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 21:42:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
    :pieces => 4,
    :rent_in_dollars => {
      :one_piece_owned => 25,
-     :two_piece_owned => 200,
+     :two_piece_owned => 50,
      :three_piece_owned => 300,
      :four_piece_owned => 400
      
