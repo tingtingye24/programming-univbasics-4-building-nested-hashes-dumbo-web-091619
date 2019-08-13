b1a90adf91633ec552340e29b78b06417cc1f02b
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 21:46:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,8 +41,6 @@ def monopoly_with_third_tier
      
    }
   }
-    
-    
   }
 end
 
@@ -51,4 +49,31 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monoply = {:railroads => {
+   :pieces => 4,
+   :rent_in_dollars => {
+     :one_piece_owned => 25,
+     :two_pieces_owned => 50,
+     :three_pieces_owned => 100,
+     :four_pieces_owned => 200
+     
+   },
+   :names => {
+     :reading_railroad => {
+       :mortgage_value => "$100"
+     },
+     :pennsylvania_railroad => {
+       :mortgage_value => "$100"
+     },
+     :b_and_o_railroad => {
+       :mortgage_value => "$100"
+     },
+     :shortline_railroad => {
+       :mortgage_value => "$100"
+     }
+     
+     
+   }
+  }
+  }
 end
