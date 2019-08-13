46685806cd5431d6722ec0c205b5183ced0f663e
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 21:25:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	monoply = {:railroads => "player1"}
+	monoply = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
