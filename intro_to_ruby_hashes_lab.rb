6cd5b1db23108a2a6962f3c0b3b9880a76354eaf
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 21:47:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,13 +63,13 @@ def monopoly_with_fourth_tier
        "mortgage_value" => "$100"
      },
      :pennsylvania_railroad => {
-       "mortgage_value" => "$100"
+       "mortgage_value" => "$200"
      },
      :b_and_o_railroad => {
-       "mortgage_value" => "$100"
+       "mortgage_value" => "$300"
      },
      :shortline_railroad => {
-       "mortgage_value" => "$100"
+       "mortgage_value" => "$100==="
      }
      
      
