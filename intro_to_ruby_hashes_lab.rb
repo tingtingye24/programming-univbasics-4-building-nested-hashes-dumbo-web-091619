d8f7f59da8c9b951432b5adce6369575b697691d
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 21:28:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monoply = {:railroads => {
-   :pieces => 3
+   :pieces => 4
   }
     
     
